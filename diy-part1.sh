dde319b34c1d7674b1e445d05d4f3c8037f99187
SandiT.T <27389078+SandiTT@users.noreply.github.com>
2021-12-23 04:57:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,5 +20,5 @@
 git clone https://github.com/AdguardTeam/AdGuardHome
 
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git adguardTeam https://github.com/AdguardTeam/AdGuardHome' >>feeds.conf.default
