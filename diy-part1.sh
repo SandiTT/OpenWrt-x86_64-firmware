fbaacebd1fa1ea5586a8759e87ab8995fe980358
SandiT.T <27389078+SandiTT@users.noreply.github.com>
2021-12-23 04:54:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,8 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
+git clone https://github.com/AdguardTeam/AdGuardHome
+
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git adguardTeam https://github.com/AdguardTeam/AdGuardHome' >>feeds.conf.default
